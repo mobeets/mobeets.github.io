f8a3b8659837237a69ae9dc0532f5c32f6b70f14
mobeets <mobeets@gmail.com>
2014-08-14 23:06:17 -0500

    well, that was fun--NOT

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,9 @@
-$(function() {
-    $('#contacts').hide();
-    $('#escapepad').click(function(){
-        if ( $("#contacts").is(":hidden") ) {
-            $('#contacts').slideDown("slow");
-        } else {
-            $('#contacts').slideUp();
-        }
+function init() {
+    $('#escapepad').click(function () {
+        justHid = false;
+        if ( $('#escapepad-label').is(':visible') ){ $('#escapepad-label').hide(); $('#escapepad-open').hide(); $('#escapepad-close').show(); justHid = true; }
+        $('#contacts').toggle('slide', { direction: 'right' }, 400, function () { if ( !justHid ){ $('#escapepad-label').show(); $('#escapepad-open').show(); $('#escapepad-close').hide(); } });
     });
-});
+}
+
+$(document).ready(init);
