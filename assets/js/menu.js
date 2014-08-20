114dfc8cc29b9f795a0c50773dd27b89b774a1bc
mobeets <mobeets@gmail.com>
2014-08-19 21:51:01 -0500

    banner styled better on posts

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,19 @@
 function init() {
     $('#escapepad').click(function () {
         justHid = false;
-        if ( $('#escapepad-label').is(':visible') ){ $('#escapepad-label').hide(); $('#escapepad-open').hide(); $('#escapepad-close').show(); justHid = true; }
-        $('#contacts').toggle('slide', { direction: 'right' }, 400, function () { if ( !justHid ){ $('#escapepad-label').show(); $('#escapepad-open').show(); $('#escapepad-close').hide(); } });
+        if ( $('#escapepad-label').is(':visible') ){ 
+            $('#escapepad-label').hide();
+            $('#escapepad-open').hide();
+            $('#escapepad-close').show();
+            justHid = true;
+        }
+        $('#contacts').toggle('slide', { direction: 'right' }, 400, function () {
+            if ( !justHid ){
+                $('#escapepad-label').show();
+                $('#escapepad-open').show();
+                $('#escapepad-close').hide();
+            }
+        });
     });
 }
-
 $(document).ready(init);
