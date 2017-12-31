ed481ccd8cc5351e3b28cde84346242c179b71aa
mobeets <mobeets@gmail.com>
2017-12-31 15:08:02 -0500

    end of year lists

--- assets/js/seen-tried-tasted.js
+++ assets/js/seen-tried-tasted.js
@@ -1,6 +1,6 @@
 var years = new Array();
-years['misc'] = [2013, 2014, 2015, 2016];
-var year = 2016;
+years['misc'] = [2013, 2014, 2015, 2016, 2017];
+var year = 2017;
 var list = 'misc';
 
 function init() {
