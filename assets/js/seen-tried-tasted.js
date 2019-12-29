337267b5a2b2121698fc09ceca22a51286eb18fb
mobeets <mobeets@gmail.com>
2019-12-28 21:57:42 -0500

    list-misc-2019

--- assets/js/seen-tried-tasted.js
+++ assets/js/seen-tried-tasted.js
@@ -1,6 +1,6 @@
 var years = new Array();
-years['misc'] = [2013, 2014, 2015, 2016, 2017, 2018];
-var year = 2018;
+years['misc'] = [2013, 2014, 2015, 2016, 2017, 2018, 2019];
+var year = 2019;
 var list = 'misc';
 
 function init() {
