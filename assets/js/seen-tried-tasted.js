2fc2565d310553832f7113dea57a17d436620326
Jay H <mobeets@gmail.com>
2024-01-02 09:48:28 -0500

    2023 lists

--- assets/js/seen-tried-tasted.js
+++ assets/js/seen-tried-tasted.js
@@ -1,6 +1,6 @@
 var years = new Array();
-years['misc'] = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021];
-var year = 2021;
+years['misc'] = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021, 2022];
+var year = 2022;
 var list = 'misc';
 
 function init() {
